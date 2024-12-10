fbf24f3251148d3d943cff8b9668b4c3f6fdb059
Edun Omatseye <edunomatseye@yahoo.com>
2024-12-11 00:32:31 +0100

    refactor(core): bump the quality of image from w185 to original quality

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
-export const POSTER_SIZE = 'w185';
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
+export const POSTER_SIZE = "original";
 
-export const getImageUrl = (path: string) => `${IMAGE_BASE_URL}/${POSTER_SIZE}${path}`;
+export const getImageUrl = (path: string) =>
+  `${IMAGE_BASE_URL}/${POSTER_SIZE}${path}`;
